a27a4805dfe70a043fb376c55c768f94bd449c72
Nishant Sinha <nishnha@gmail.com>
2018-05-01 18:29:20 -0400

    Created Datalog.java with main method

---
+++ gen/Datalog.java
@@ -0,0 +1,15 @@
+import datalog.generated;
+import org.antlr.runtime.*;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+public class Datalog extends DatalogBaseListener {
+    public static void main(String[] args) throws Exception {
+        ANTLRStringStream in = new ANTLRStringStream("12*(5-6)");
+        ExpLexer lexer = new ExpLexer(in);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        ExpParser parser = new ExpParser(tokens);
+        System.out.println(parser.eval()); // print the value
+    }
+}
